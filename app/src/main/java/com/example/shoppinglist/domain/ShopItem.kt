4b66009b3f0c0a94779b291a06018f75657090a1
Maksim <mccookdeveloper@gmail.com>
2022-10-15 00:41:29 +0300

    Создание дата слоя

--- app/src/main/java/com/example/shoppinglist/domain/ShopItem.kt
+++ app/src/main/java/com/example/shoppinglist/domain/ShopItem.kt
@@ -3,9 +3,13 @@ package com.example.shoppinglist.domain
 
 
 data class ShopItem(
-    val id: Int,
     val name: String,
     val count: Float,
-    val enabled: Boolean
-)
+    val enabled: Boolean,
+    var id: Int = UNDEFINED_ID
+) {
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
 
